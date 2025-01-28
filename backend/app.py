4d0eb893e1d50abe9ba6df31ae3d899fe3524962
sidz111 <sssurwade2212@gmail.com>
2025-01-28 12:06:00 +0530

    upate app.py file for deployment

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, send_file
 from downloader import download_video
+import os
 
 app = Flask(__name__)
 
@@ -19,4 +20,6 @@ def download():
     return send_file(filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use the PORT environment variable for the port and bind to 0.0.0.0
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT isn't set
+    app.run(host='0.0.0.0', port=port, debug=True)
