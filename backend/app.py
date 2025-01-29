b28a19b743ef808dd18d33539f1db67cac4bdca9
sidz111 <sssurwade2212@gmail.com>
2025-01-29 21:06:36 +0530

    changes for deployment on server

--- backend/app.py
+++ backend/app.py
@@ -21,4 +21,4 @@ def download():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Automatically bind to the correct port in production
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
